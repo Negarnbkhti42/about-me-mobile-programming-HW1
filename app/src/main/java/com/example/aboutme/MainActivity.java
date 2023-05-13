95a1fe12dbf5f511bd3ec6798440cf248516e260
Negarnbkhti42 <norob35@gmail.com>
2023-05-13 11:25:33 +0430

    Setup project

---
+++ app/src/main/java/com/example/aboutme/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.aboutme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
